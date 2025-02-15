ace23a3da633b6fb1e7b2c65256b88319aadb7ce
Dennison Bertram <dennison@dennisonbertram.com>
2025-02-14 23:16:01 -0500

    Revert "docs: Add API documentation for common topics endpoints"
    This reverts commit 66237f13b09022dbc25acde8f96403c27927bf7c.

--- scripts/enable-pgvector.sql
+++
@@ -1,5 +0,0 @@
--- Enable pgvector extension for vector similarity search
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Verify the extension is installed
-SELECT * FROM pg_extension WHERE extname = 'vector'; 
